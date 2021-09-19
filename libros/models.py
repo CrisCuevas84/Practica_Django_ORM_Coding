e1728c7e505ba0b08e8886bf2366d4df60580146
CrisCuevas84 <ccuevas84@ug.uchile.cl>
2021-09-18 21:02:38 -0300

    creando modelos Autor y Libro

--- libros/models.py
+++ libros/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.db.models.fields import CharField
 
 # Create your models here.
+class Autor(models.Model):
+    nombre = models.CharField(max_length=60)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+class Libro(models.Model):
+    titulo = models.CharField(max_length=60)
+    autor = models.ForeignKey(Autor, related_name= "libros", on_delete= models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    
